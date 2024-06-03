d2497b39b87963d6d8bd18c8781812f3bfe2a609
임민영 <raccoon@shinsegae.com>
2024-06-03 14:17:34 +0900

    git init

---
+++ src/main/java/com/dddscaffolding/DddScaffoldingApplication.java
@@ -0,0 +1,13 @@
+package com.dddscaffolding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddScaffoldingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddScaffoldingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dddscaffolding/DddScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dddscaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddScaffoldingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
